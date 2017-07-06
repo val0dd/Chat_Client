4e18a32617799605cab0394cc4f689cb9c3b201b
0ddlyoko <nathangiaco@hotmail.com>
2017-07-06 16:08:26 +0200

    Adding BufferConnection class, which contain all bytes which will be sent over the Internet, and creating a Test class

--- src/me/valodd/chatclient/MainClient.java
+++ src/me/valodd/chatclient/MainClient.java
@@ -1,10 +1,12 @@
 package me.valodd.chatclient;
 
+import me.valodd.chatclient.test.Test;
+
 public class MainClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Test();
 	}
 
 }
